8ebec173e03f72b1ff79c9ec52a41781ad13cc89
Raghav <rcherukuru@ucsd.edu>
2021-10-15 16:48:14 -0700

    part 2 done

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,3 +1,7 @@
-let d = new Date();
-let time = d.toLocaleTimeString();
-console.log(time);
+
+function clock() {
+    let d = new Date();
+    let time = d.toLocaleTimeString();
+    console.log(time);
+}
+setInterval(clock,1000);
